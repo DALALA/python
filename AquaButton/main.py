3f3404e4c9f52d734b46330c82ece78fdd06d917
RobertChen <RobertChen@hyenatek.com>
2014-07-09 18:54:14 +0800

    add colour value of the colour select in main panel tuning the layout

--- AquaButton/main.py
+++ AquaButton/main.py
@@ -5,12 +5,19 @@ from Preference_Urban import *
 ColorList = ["BackgroundColour",
              "DisabledColour",
              "ForegroundColour",
+             "ShadowColour",
              "HoverColour",
+             "GraidentColour",
              "RectColour"]
 class MyAquaButton(AB.AquaButton):
     def __init__(self, *args, **kwargs ):
         AB.AquaButton.__init__(self,*args, **kwargs)
-        self.SetForegroundColour(wx.BLACK)
+        self._gradientColour = wx.WHITE
+    def GetGraidentColour(self):
+        return self._gradientColour
+    def SetGraidentColour(self,colour):
+        self._gradientColour = colour
+        self.Invalidate()
     def OnPaint(self, event):
         """
         Handles the ``wx.EVT_PAINT`` event for :class:`AquaButton`.
@@ -22,7 +29,7 @@ class MyAquaButton(AB.AquaButton):
         gc = wx.GraphicsContext.Create(dc)
 
         xpos, ypos, width, height = self.GetClientRect()
-        print xpos, ypos, width, height 
+         
         dc.SetBackground(wx.Brush(self._rectColour))
         dc.Clear()
         gc.SetBrush(wx.WHITE_BRUSH)
@@ -44,7 +51,7 @@ class MyAquaButton(AB.AquaButton):
 
         rc1 = wx.Rect(btnOffset, btnOffset, width-8-btnOffset, height-8-btnOffset)        
         path1 = self.GetPath(gc, rc1, 10)
-        br1 = gc.CreateLinearGradientBrush(0, 0, 0, rc1.height+6, clr, wx.WHITE)
+        br1 = gc.CreateLinearGradientBrush(0, 0, 0, rc1.height+6, clr, self._gradientColour)
 
         # Create shadow
         rc2 = wx.Rect(*rc1)
@@ -52,16 +59,16 @@ class MyAquaButton(AB.AquaButton):
         path2 = self.GetPath(gc, rc2, 10)
         br2 = gc.CreateRadialGradientBrush(rc2.x, rc2.y,
                                            rc2.x+rc2.width, rc2.y+rc2.height,
-                                           rc2.width, self._shadowColour, wx.WHITE)
+                                           rc2.width, self._shadowColour, self._gradientColour)
 
         # Create top water colour to give "aqua" effect
         rc3 = wx.Rect(*rc1)
         rc3.Inflate(-5, -5)
         rc3.height = 15
         path3 = self.GetPath(gc, rc3, 10)
-
+        r,g,b = self._gradientColour.Get()
         br3 = gc.CreateLinearGradientBrush(rc3.x, rc3.y, rc3.x, rc3.y+rc3.height,
-                                           wx.Colour(255, 255, 255, 255), wx.Colour(255, 255, 255, 0))
+                                           wx.Colour(r, g, b, 255), wx.Colour(r, g, b, 0))
 
         # draw shapes
         gc.SetBrush(br2)
@@ -76,12 +83,12 @@ class MyAquaButton(AB.AquaButton):
         gc.SetFont(font)
         label = self.GetLabel()
         tw, th = gc.GetTextExtent(label)
-        print tw,th
+
         if self._bitmap:
             bw, bh = self._bitmap.GetWidth(), self._bitmap.GetHeight()
         else:
             bw = bh = 0
-        print bw,bh
+
         '''
         pos_x = (width-bw-tw)/2+btnOffset-shadowOffset      # adjust for bitmap and text to centre
         if self._bitmap:
@@ -95,11 +102,9 @@ class MyAquaButton(AB.AquaButton):
         pos_y = (height-bh-th)/2+btnOffset-shadowOffset      # adjust for bitmap and text to centre
         if self._bitmap:
             pos_x =  (width-bw-shadowOffset)/2+btnOffset
-            print "pic:",pos_x,pos_y
             gc.DrawBitmap(self._bitmap, pos_x, pos_y, bw, bh) # draw bitmap if available
             pos_y = pos_y + 2   # extra spacing from bitmap
         
-        print "txt:",(width-tw-shadowOffset)/2+btnOffset,pos_y + bw + btnOffset
         # Create a Path to draw the text
         gc.DrawText(label, (width-tw-shadowOffset)/2+btnOffset, pos_y + bw + btnOffset)      # draw the text
         #'''
@@ -162,31 +167,57 @@ class MyAquaButton(AB.AquaButton):
             
 class MyFrame ( wx.Frame ):
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = "AquaButton Attribute Setting", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        ft = self.GetFont()
+        ft.SetPointSize(12)
+        ft.SetFaceName(u"Eras Medium ITC")
+        self.SetFont(ft)        
         self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
         self.SetBackgroundColour(wx.WHITE)
         gbSizer = wx.GridBagSizer( 0, 0 )
         gbSizer.SetFlexibleDirection( wx.BOTH )
         gbSizer.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
         self.btn1 = MyAquaButton(self, wx.ID_ANY, Preference_Urban.GetBitmap(), "AquaButton")
+        self.btn1.SetForegroundColour(wx.BLACK)        
         h=0
         for attr in ColorList:
             setattr(self, "m_"+attr, wx.ColourPickerCtrl(self , col=getattr(self.btn1,"Get"+attr)()))
-            gbSizer.Add( wx.StaticText( self, wx.ID_ANY, attr), wx.GBPosition( h, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.ALIGN_CENTER, 5 )
-            gbSizer.Add( getattr(self,"m_"+attr)              , wx.GBPosition( h, 2 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.ALIGN_CENTER, 5 )
+            gbSizer.Add( wx.StaticText( self, wx.ID_ANY, attr), wx.GBPosition( h, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+            gbSizer.Add( getattr(self,"m_"+attr)              , wx.GBPosition( h, 2 ), wx.GBSpan( 1, 1 ), wx.ALL | wx.ALIGN_RIGHT, 5 )
+            gbSizer.Add( wx.StaticText( self, wx.ID_ANY, getattr(self.btn1,"Get"+attr)().GetAsString(wx.C2S_HTML_SYNTAX)), wx.GBPosition( h, 3 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.ALIGN_CENTER, 5 )
             h += 1
         self.m_cbPulseOnFocus = wx.CheckBox(self, wx.ID_ANY, "Pulse On Press")
-        gbSizer.Add( self.m_cbPulseOnFocus, wx.GBPosition( h, 1 ), wx.GBSpan( 1, 2 ), wx.ALL|wx.ALIGN_LEFT, 5 )
+        gbSizer.Add( self.m_cbPulseOnFocus, wx.GBPosition( h, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.ALIGN_LEFT, 5 )
         h += 1
         self.m_cbDisable = wx.CheckBox(self, wx.ID_ANY, "Disable Button")
-        gbSizer.Add( self.m_cbDisable, wx.GBPosition( h, 1 ), wx.GBSpan( 1, 2 ), wx.ALL|wx.ALIGN_LEFT, 5 )
+        gbSizer.Add( self.m_cbDisable, wx.GBPosition( h, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.ALIGN_LEFT, 5 )
         h += 1
-        gbSizer.Add( self.btn1, wx.GBPosition( 0, 0 ), wx.GBSpan( h, 1 ), wx.ALL, 5 )
+        self.m_PointSize = wx.ComboBox( self, choices = ["8","9","10","11","12","14","16","18","20","22","24","26","28","36","48","72"],style = wx.TE_PROCESS_ENTER )
+        gbSizer.Add( wx.StaticText( self, wx.ID_ANY, "PointSize"), wx.GBPosition( h, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        gbSizer.Add( self.m_PointSize                            , wx.GBPosition( h, 2 ), wx.GBSpan( 1, 2 ), wx.ALL|wx.EXPAND, 5 )
+        self.m_PointSize.SetValue(str(self.btn1.GetFont().GetPointSize()))
+        h += 1
+        e = wx.FontEnumerator()
+        e.EnumerateFacenames()
+        flist = e.GetFacenames()
+        flist.sort()
+        self.m_FaceName = wx.ComboBox( self, choices = flist,style = wx.TE_PROCESS_ENTER)
+        gbSizer.Add( wx.StaticText( self, wx.ID_ANY, "FaceName"), wx.GBPosition( h, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        gbSizer.Add( self.m_FaceName                            , wx.GBPosition( h, 2 ), wx.GBSpan( 1, 2 ), wx.ALL|wx.ALIGN_CENTER, 5 )
+        self.m_FaceName.SetValue(str(self.btn1.GetFont().GetFaceName()))
+        h += 1
+        
+        gbSizer.Add( self.btn1, wx.GBPosition( 0, 0 ), wx.GBSpan( h, 1 ), wx.ALL|wx.ALIGN_CENTER, 5 )
         self.SetSizer( gbSizer )
         self.Layout()
+        self.SetClientSize(self.GetEffectiveMinSize())
         self.Centre( wx.BOTH )
         self.Bind(wx.EVT_COLOURPICKER_CHANGED, self.OnPickColour)
-        self.Bind(wx.EVT_CHECKBOX, self.OnCheck)
+        self.Bind(wx.EVT_CHECKBOX    , self.OnCheck)
+        self.m_PointSize.Bind(wx.EVT_COMBOBOX    , self.OnChoice)
+        self.m_PointSize.Bind(wx.EVT_TEXT_ENTER  , self.OnChoice)
+        self.m_FaceName.Bind(wx.EVT_COMBOBOX    , self.OnChoice)
+        self.m_FaceName.Bind(wx.EVT_TEXT_ENTER  , self.OnChoice)
         self.OnCheck(None)
     def OnPickColour(self, event):
         for attr in ColorList:
@@ -195,6 +226,14 @@ class MyFrame ( wx.Frame ):
     def OnCheck(self, event):
         self.btn1.SetPulseOnFocus(self.m_cbPulseOnFocus.IsChecked())
         self.btn1.Enable(not self.m_cbDisable.IsChecked())
+    def OnChoice(self, event):
+        ft = self.btn1.GetFont()
+        ft.SetPointSize(int(self.m_PointSize.GetValue()))
+        ft.SetFaceName(self.m_FaceName.GetValue())
+        self.btn1.SetFont(ft)
+        self.btn1.Refresh()
+        self.Layout()
+        self.SetClientSize(self.GetEffectiveMinSize())
 if __name__=='__main__':
     #'''
     app = wx.App()
